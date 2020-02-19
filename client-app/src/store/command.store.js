473ec28033ceffe238025c1e9364a4acdf56c479
Tanvir <tanvirislam.cs@gmail.com>
2020-02-19 00:17:36 -0500

    command history

--- client-app/src/store/command.store.js
+++ client-app/src/store/command.store.js
@@ -13,68 +13,16 @@ const getters = {
 
 // MUTATIONS
 const mutations = {
-    add(state, cmd) {
-        state.queue.push(cmd);
-        state.history.push(cmd);
-    },
-
-    pop(state) {
-        return state.history.pop();
-    },
-
-    isBeingProcessed(state, commandStr) {
-        let commandObj = null;
-
-        for (let i = 0; i < state.history.length; ++i) {
-            if (history[i].command === commandStr) {
-                commandObj = history[i];
-                break;
-            }
-        }
-
-        if (commandObj !== null) {
-            return (commandObj.hasExecuted === false && state.queue.includes(commandObj));
-        }
-        else {
-            return false;
-        }
-    },
-
-    hasBeenProcessed(state, commandStr) {
-        let commandObj = null;
-
-        for (let i = 0; i < state.history.length; ++i) {
-            if (history[i].command === commandStr) {
-                commandObj = history[i];
-                break;
-            }
-        }
-
-        if (commandObj !== null) {
-            return (commandObj.hasExecuted === true && !state.queue.includes(commandObj));
-        }
-        else {
-            return false;
-        }
+    add(state, commandObj) {
+        state.queue.push(commandObj);
+        state.history.push(commandObj);
     }
 };
 
 // ACTIONS
 const actions = {
-    add(context, cmd) {
-        context.commit('add', cmd);
-    },
-
-    pop(context) {
-        return context.commit('pop');
-    },
-
-    isBeingProcessed(context, commandStr) {
-        return context.commit('isBeingProcessed', commandStr);
-    },
-
-    hasBeenProcessed(context, commandStr) {
-        return context.commit('hasBeenProcessed', commandStr);
+    add(context, commandObj) {
+        context.commit('add', commandObj);
     }
 };
 
