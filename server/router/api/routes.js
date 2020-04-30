11a6d3b6854c872df9968d512d11c42894b42419
mislam <md.islam@factset.com>
2020-04-30 18:45:27 -0400

    moving code to devel

--- server/router/api/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (request, response) => {
-    let msg = { "name": "gitfiddle", "description": "git branching visualization tool" };
-    response.send(JSON.stringify(msg));
-});
-
-module.exports = router;
