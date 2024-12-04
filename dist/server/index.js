2082dd5ff93b13774e21a1ae95f8f4d865907887
tanvirmislam <tanvirislam.cs@gmail.com>
2024-12-03 21:33:33 -0500

    vercel

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-
 const cors = require('cors');
-
 const path = require('path');
-
 const history = require('connect-history-api-fallback');
-
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(cors()); // Client app routes redirection
+app.use(cors());
 
+// Client app routes redirection
 const publicRoot = path.resolve(__dirname, '../public');
 console.log(publicRoot);
 app.use(express.static(publicRoot));
